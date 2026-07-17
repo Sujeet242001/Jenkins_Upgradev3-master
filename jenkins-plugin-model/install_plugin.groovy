896a905dfafeca4ad5d68dee404609f5838c2b8d
sujeet <sujeetkale2001@gmail.com>
2026-07-17 16:25:19 +0530

    Initial commit

--- jenkins-plugin-model/install_plugin.groovy
+++ jenkins-plugin-model/install_plugin.groovy
@@ -16,3 +16,37 @@ pm.doCheckUpdatesServer()
 
 // Restart Jenkins after installing plugins (optional)
 Jenkins.instance.restart()
+
+// import jenkins.model.Jenkins
+
+// def jenkins = Jenkins.get()
+
+// def pm = jenkins.pluginManager
+// def uc = jenkins.updateCenter
+
+// // Refresh update center metadata
+// uc.updateAllSites()
+
+// def plugins = [
+//     "github",
+//     "mstest",
+//     "workflow-aggregator",
+//     "docker-build-publish"
+// ]
+
+// plugins.each { pluginName ->
+//     if (pm.getPlugin(pluginName) == null) {
+//         def plugin = uc.getPlugin(pluginName)
+//         if (plugin != null) {
+//             println "Installing ${pluginName}..."
+//             plugin.deploy(true).get()
+//         } else {
+//             println "Plugin ${pluginName} not found in Update Center."
+//         }
+//     } else {
+//         println "Plugin ${pluginName} is already installed."
+//     }
+// }
+
+// // Restart Jenkins if required
+// jenkins.safeRestart()
